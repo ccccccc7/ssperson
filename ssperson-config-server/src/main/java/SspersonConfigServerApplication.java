5ca328fabbda8f3f1803d1616898823dd885cc72
张逸霖 <869937739@qq.com>
2019-01-16 11:10:13 +0800

    配置中心

---
+++ ssperson-config-server/src/main/java/SspersonConfigServerApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * the config server.
+ * @author zyl
+ */
+
+@SpringBootApplication
+public class SspersonConfigServerApplication {
+}
