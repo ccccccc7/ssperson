f5ba3cfd5318564afaec2050ea9ab1597b57a7cc
张逸霖 <869937739@qq.com>
2019-01-16 17:52:31 +0800

    消息总线

--- ssperson-config-server/README.md
+++ ssperson-config-server/README.md
@@ -1,1 +1,11 @@
 #配置中心
+
+##消息总线
+安装RabiitMq [windows版](http://www.rabbitmq.com/install-windows.html)  
+rabbitmq-plugins enable rabbitmq_management rabbitmq管理界面 默认账号密码guest  
+4369 -- erlang发现口  
+5672 -- client端通信口  
+15672 -- 管理界面ui端口  
+25672 -- server间内部通信
+
+http://localhost:7005/actuator/bus-refresh  POST方法刷新消息总线上配置
--- ssperson-provider/ssperson-provider-crm/src/main/java/com/ly/controller/IndexController.java
+++ ssperson-provider/ssperson-provider-crm/src/main/java/com/ly/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.ly.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author zyl
  */
+@RefreshScope
 @RequestMapping(value = "")
 @RestController
 public class IndexController {
