92db688f51c584217aa574a72edf87bfd284f64c
zyl <869937739@qq.com>
2019-01-16 14:03:21 +0800

    配置中心

---
+++ ssperson-config-server/src/main/java/com/ly/config/server/SspersonConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.ly.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * the config server.
+ * @author zyl
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class SspersonConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SspersonConfigServerApplication.class, args);
+    }
+}
--- ssperson-provider/ssperon-provider-crm/src/main/java/com/ly/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ly.service;
-
-/**
- * @author zyl
- */
-public interface UserService {
-}
